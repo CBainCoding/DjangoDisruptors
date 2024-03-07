8dc2b248ab47658e5e0730db810f2cb61a421ac5
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-07 18:07:00 +0000

    Debug to true for heroku testing

--- charlestest/settings.py
+++ charlestest/settings.py
@@ -99,17 +99,7 @@ WSGI_APPLICATION = 'charlestest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if "DEBUG" in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DB_URL"))
-    }
+DEBUG = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
